83a8301ed2592c7e404bfec8c06295e6e8bfee0e
gamesam88 <dyxpp@rambler.com>
2023-01-08 02:29:58 +0300

    feat: add redux-persist for saving cart

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import products from "./slices/productsSlice";
 import filter from "./slices/filterSlice";
 import cart from "./slices/cartSlice";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-export const store = configureStore({
-  reducer: {
-    filter,
-    products,
-    cart,
-  },
+const rootReducer = combineReducers({ filter, products, cart });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+export default store;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
