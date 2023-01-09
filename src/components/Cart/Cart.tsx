db74bb43650f7a6ec82294899e835d1453c82984
gamesam88 <dyxpp@rambler.com>
2023-01-09 16:21:19 +0300

    feat: add promo

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Cart.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
@@ -10,6 +10,24 @@ const Cart = () => {
   const { productsInCart, totalAmount, totalPrice } = useSelector((state: RootState) => state.cart);
   const [cartElements, setCartElements] = useState<ProductInCartType[]>();
   const dispatch = useDispatch();
+
+  const [promoValue, setpromoValue] = useState<string>("");
+  const [discount, setDiscount] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setpromoValue(event.target.value);
+  };
+
+  const finalPrice = discount ? totalPrice * 0.9 : totalPrice;
+
+  useEffect(() => {
+    if (promoValue === "PROMO") {
+      setDiscount(true);
+    } else {
+      setDiscount(false);
+    }
+  }, [promoValue]);
 
   useEffect(() => {
     setCartElements(productsInCart);
@@ -40,7 +58,11 @@ const Cart = () => {
             <p>Количество товара:</p>
             <span>{totalAmount} шт.</span>
             <p>Цена всех товаров:</p>
-            <span>{totalPrice} $</span>
+            <span>{finalPrice} $</span>
+            {discount ? <p className="payment__text-discount">Скидка: -10%</p> : ""}
+          </div>
+          <div className="discount">
+            <input ref={inputRef} value={promoValue} type="text" placeholder="PROMO" onChange={onChangeInput} />
           </div>
           <button
             onClick={() => {
