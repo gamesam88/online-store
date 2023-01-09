981783dc4c3c79b9d34b385581816d1461fea1b8
gamesam88 <dyxpp@rambler.com>
2023-01-09 17:20:50 +0300

    refactor: refactor product card

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import { useState } from "react";
 import "./productCard.scss";
 import { AddProductToCart } from "../addProductToCart/addProductToCard";
 import { ProductType } from "../../models/models";
 import { Link } from "react-router-dom";
-
 interface IProductProps {
   product: ProductType;
   id: number;
 }
 
 export function ProductCard({ product, id }: IProductProps) {
-  const [details, setDetails] = useState(false);
-
-  const btnClassName = details ? "add-yellow" : "add-blue";
-  const btnClasses = ["btn-class", btnClassName];
-
   const cartItem = product && {
     title: product.title,
     image: product.thumbnail,
@@ -30,24 +23,33 @@ export function ProductCard({ product, id }: IProductProps) {
   return (
     <div>
       {
-        <div className="card ">
-          <Link to={`product/${id}`}>
-            <img src={product.thumbnail} alt={product.title} className="card-image" />
-            <p>{product.title}</p>
-          </Link>
-          <span className="font-bold">{product.price}$</span>
-          <button className={btnClasses.join(" ")} onClick={() => setDetails((prev) => !prev)}>
-            {details ? "Hide details" : "Show details"}
-          </button>
-          {details && (
-            <div>
-              <p>{product.description}</p>
-              <p>
-                Rate: <span style={{ fontWeight: "bold" }}>{product.rating}</span>
-              </p>
-            </div>
-          )}
-          <AddProductToCart cartItem={cartItem} />
+        <div className="card">
+          <img src={product.thumbnail} alt={product.title} className="card-image" />
+          <h4>{product.title}</h4>
+          <div>
+            <p>
+              Цена: <span className="font-bold">{product.price}$</span>
+            </p>
+            <p>
+              Рейтинг: <span style={{ fontWeight: "bold" }}>{product.rating}</span>
+            </p>
+            <p>
+              Скидка: <span style={{ fontWeight: "bold" }}>{product.discountPercentage} %</span>
+            </p>
+            <p>
+              На складе: <span style={{ fontWeight: "bold" }}>{product.stock} шт</span>
+            </p>
+          </div>
+          <div className="card__btns-block">
+            <button className="btn btnStyle btn__addToCart">
+              {" "}
+              <Link to={`product/${id}`}>
+                {" "}
+                <span>О товаре</span>
+              </Link>
+            </button>
+            <AddProductToCart cartItem={cartItem} />
+          </div>
         </div>
       }
     </div>
